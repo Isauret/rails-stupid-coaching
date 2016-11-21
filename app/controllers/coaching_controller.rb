e123002f1022d576c9f7cdfd2b40a98d67a9b980
Isaure Troesch <isauretroesch@gmail.com>
2016-11-21 14:48:10 +0100

    stupid coaching

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,26 @@
+class CoachingController < ApplicationController
+
+  def answer
+    @query = params[:query]
+    @coach_message = coach_message(@query)
+  end
+
+  def ask
+
+
+  end
+
+
+private
+
+  def coach_message(query)
+    if query.downcase == "I am going to work right now!"
+
+    elsif query.end_with?("?")
+      "Silly question."
+    else
+      "I don't care"
+    end
+  end
+
+end
